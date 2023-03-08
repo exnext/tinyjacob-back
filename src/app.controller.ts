61c2f35e50ac052e315cbcabf32106294c38a511
jakubir <irlajakub@gmail.com>
2023-03-08 14:39:13 +0100

    Modyfikacja dostarczonych danych; Polaczenie z baza danych

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Sequelize, DataTypes } from 'sequelize';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post(':link')
+  async upload(@Body() par): Promise<string> {
+    if (par.link == '')
+      return 'Puste dane';
+
+    const sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: 'src/db/tiny-jacob.sqlite'
+    });
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+
+      const Hash = sequelize.define('Hash', {
+        link: {type: DataTypes.TEXT},
+        hash: {type: DataTypes.TEXT}
+      }, {
+        freezeTableName: true,
+        timestamps: false
+      });
+
+      const hashes = await Hash.findAll();
+      console.log(JSON.stringify(hashes));
+
+      let link: any = par.link.split('//');
+      let [, ...res] = link[link.length - 1].split('/');
+      return 'ok.pl?hash='+res.join('/').replace('?', '');
+    } catch (error) {
+      console.log(error);
+      return 'Errror';
+    }
   }
 }
